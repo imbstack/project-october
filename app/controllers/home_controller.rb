819a264318d43d606953bad200a1a7b676d2e031
Brian Stack <bis12@case.edu>
2013-01-30 00:00:07 -0500

    Add test thrift into controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
+require 'thrift'
+
+$:.push('lib/thrift/gen-rb')
+require 'recommender'
+
 class HomeController < ApplicationController
   def index
+    transport = Thrift::BufferedTransport.new(Thrift::Socket.new('localhost', 9090))
+    protocol = Thrift::BinaryProtocol.new(transport)
+    client = Recommender::Client.new(protocol)
+
+    transport.open()
+
+    @answer = client.ping()
   end
 end
