69f72809c29f83714c04baa4afacacb4f1848de2
Brian Stack <bis12@case.edu>
2013-01-29 22:30:02 -0500

    init Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectOctober::Application.config.secret_token = '9c648115e9fe9725b91daf473341c4dea8ccb1d34e69dd50d5285dba5e278dcf5937bdeba8cf9b5e1b452eed5147e699dea56cabd1ff069a313268e81b1631be'
