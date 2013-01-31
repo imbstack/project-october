e029c6a90ed05ad38be3ab0ed2ea8fcb187b0075
Brian Stack <bis12@case.edu>
2013-01-31 18:14:49 -0500

    Use different transport

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ require 'recommender'
 
 class HomeController < ApplicationController
   def index
-    transport = Thrift::BufferedTransport.new(Thrift::Socket.new('localhost', 9090))
+    transport = Thrift::FramedTransport.new(Thrift::Socket.new('localhost', 9090))
     protocol = Thrift::BinaryProtocol.new(transport)
     client = Recommender::Client.new(protocol)
 
