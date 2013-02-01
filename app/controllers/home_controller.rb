7aa12f1ab9ef3e68f0282d85d648e887dcffcba1
Tom Dooner <tom@causes.com>
2013-01-31 20:44:14 -0500

    Use 127.0.0.1 instead of localhost to appease Mac OS
    For some unknown reason, this works perfectly with 127.0.0.1 but doesn't
    work with localhost. Eh?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ require 'recommender'
 
 class HomeController < ApplicationController
   def index
-    transport = Thrift::FramedTransport.new(Thrift::Socket.new('localhost', 9090))
+    transport = Thrift::FramedTransport.new(Thrift::Socket.new('127.0.0.1', 9090))
     protocol = Thrift::BinaryProtocol.new(transport)
     client = Recommender::Client.new(protocol)
 
