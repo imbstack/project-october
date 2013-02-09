7ed5ff9f9ddb4afe2157dc282df93a137c96f500
Tom Dooner <tom@causes.com>
2013-02-09 14:33:49 -0500

    Move Thrift initialization into config/initializers/thrift.rb
    Note: This doesn't currently put thrift in the global namespace, as it
    probably should. Or, at least, in a namespace that we can use it in the
    rest of the Rails app.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,3 @@
-require 'thrift'
-
-$:.push('lib/thrift/gen-rb')
-require 'recommender'
-
 class HomeController < ApplicationController
-  def index
-    transport = Thrift::FramedTransport.new(Thrift::Socket.new('127.0.0.1', 9090))
-    protocol = Thrift::BinaryProtocol.new(transport)
-    client = Recommender::Client.new(protocol)
-
-    transport.open()
-
-    @answer = client.ping()
-  end
+  def index; end
 end
---
+++ config/initializers/thrift.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH << 'lib/thrift/gen-rb'
+
+require 'thrift'
+require 'thrift/gen-rb/recommender'
+
+begin
+  # Note: for now, these variables are inaccessible by the rest of the
+  # application. TODO: rectify this when we start adding more api calls to
+  # thrift
+  transport = Thrift::FramedTransport.new(Thrift::Socket.new('127.0.0.1', 9090))
+  protocol = Thrift::BinaryProtocol.new(transport)
+  client = Recommender::Client.new(protocol)
+
+  transport.open()
+rescue Thrift::TransportException => e
+  Rails.logger.error "Cannot Connect to Thrift Backend"
+end
+
