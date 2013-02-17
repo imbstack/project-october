975098b97cadb7197c89d2c1bfd3be7d6e979fbc
Brian Stack <bis12@case.edu>
2013-02-17 18:19:09 -0500

    Validate usernames

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,12 +4,38 @@ class UserTest < ActiveSupport::TestCase
 
   test "names are unique" do
     User.create(:name => "duplicate")
-    assert_raise ActiveRecord::RecordNotUnique do
-      User.create(:name => "duplicate")
+    u = User.new(:name => "duplicate")
+    if u.save
+      assert false, "User was saved with duplicate name"
+    else
+      assert_includes u.errors.messages[:name], "This username already exists"
     end
   end
 
   test "names contain no spaces" do
-    assert false
+    u = User.new(:name => "in space, no one")
+    if u.save
+      assert false, "Whitespace was allowed in username"
+    else
+      assert_includes u.errors.messages[:name], "Usernames cannot have spaces in them"
+    end
+  end
+
+  test "name cannot be too short" do
+    u = User.new(:name => "a")
+    if u.save
+      assert false
+    else
+      assert_includes u.errors.messages[:name], "Usernames must have at least 3 characters"
+    end
+  end
+
+  test "name cannot be too long" do
+    u = User.new(:name => "thisnamehastoomanycharacters!")
+    if u.save
+      assert false
+    else
+      assert_includes u.errors.messages[:name], "Usernames cannot have less than 25 characters"
+    end
   end
 end
