fb7f61742d2056e3aa63476d8b2e13db5ba71e43
Tom Dooner <tom@causes.com>
2013-02-28 20:24:19 -0500

    WIP

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,16 +4,16 @@ class Post < ActiveRecord::Base
   attr_accessible :title
   # TODO: Remove this bit once we get real data
   FAKE_HEADLINES = [
-    'Desperate GOP reaches out to Sauron, Voldemort',
-    'Saints coach Sean Payton suspended from coaching for one year',
-    'Gingrich vows to keep going, looks for extra fifty states',
-    'GOP punches self in eye, blames Dems',
-    'Romney campaign announces plans to purchase Michigan',
+    'Insatiable Water Droplet Barrels Down Windowpane Consuming Everything In Its Path',
+    'Iran Promises To End Nuclear Program In Exchange For Detailed Diagram Of Atomic Bomb',
+    'Danica Patrick Flooded With Fan Mail From Nation\'s Inspired Little Girl',
+    'Obama, Congress Must Reach Deal On Budget By March 1, And Then April 1, And Then April 20, And Then April 28, And Then May 1',
+    'Area Man Relieved To Hear State Of Union Still Strong',
     'Adele wins “Best Instrumental” and “Best Spoken Word Performance”',
     'Company’s sole black employee wishes coworkers would stop trying to talk to him about black history month',
-    'Unemployment drops for fifth straight month, as economy continues to lean Democratic',
-    'Gingrich campaign strategy to involve last-minute death ray construction',
-    'Obama begins state of union speech decrying lack of DiCaprio nomination',
+    'Woman Rushed Into Cosmetic Surgery With 8 Glaring Flaws',
+    "'Les Misérables' Takes Home Oscar For Most Sound",
+    '240 Killed In Stampede After Bucketful Of Oscars Just Dumped On Stage',
     'Words With Friends addict keeps trying to use “AE”, “AA”, “XI”, “QI” and “JO” in a sentence'
   ]
 
