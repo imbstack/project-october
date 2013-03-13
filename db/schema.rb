899c541f28ec0c17a0ceff52ae1983ff3c6bf5da
Tom Dooner <tom@causes.com>
2013-03-13 15:07:48 -0400

    Add URL to posts
    Let's store the original URL for each post

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221185507) do
+ActiveRecord::Schema.define(:version => 20130313190453) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "url"
   end
 
   create_table "users", :force => true do |t|
