695e129bcaf2f99fbf66ef438421a638b4065958
Tom Dooner <tom@causes.com>
2013-03-14 22:13:00 -0400

    Post display page, including the idea of Comments
    This wires up the post show page. It's a bit meager right now, but I
    wanted to take a crack at the most interesting part -- the comment
    system.
    
    To this end I have found an interesting library 'awesome_nested_set'
    which is way more interesting than just setting a parent_id on the
    comments to refer to their parents. You still have to set a parent_id,
    but it uses the IDs of comments in a clever way so it improves the
    efficiency of some operations. Namely
    -- you can select all descendants of a comment: one SQL query
    -- deleting a comment and its descendants: one query
    -- more benefits here:
      http://threebit.net/tutorials/nestedset/tutorial1.html
    (The downside is that the SQL for inserting a comment is pretty
    ridiculous. Not a lot of queries -- but one very complicated query.
    Fortunately, we can assume it's well tested).

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.11'
 gem 'haml'
 gem 'sass' # rake was throwing errors without this
 gem 'devise'
+gem 'awesome_nested_set'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313190453) do
+ActiveRecord::Schema.define(:version => 20130314200955) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
