bb3d0da298340457a5a34546819f2ea14dc8bdd3
Tom Dooner <tom@causes.com>
2013-03-25 03:07:58 -0400

    Style changes for the homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,33 +9,37 @@ class PostsController < ApplicationController
 
   def create
     begin
-      src = open(params[:post][:url]).read
+      post = Pismo::Document.new(params[:post][:url])
+      images = post.images
+      title = post.title
+      leader = post.lede # This is the first couple sentences.
+      keywords = post.keywords(
+        :minimum_score => 1,
+        :stem_at => 2,
+        :word_length_limit => 30,
+        :limit => 500
+      )
     rescue
       return redirect_to new_post_url, :flash => {
-        :error => 'Could not fetch article.'
+        :error => "Could not fetch article!"
       }
     end
-    post = Pismo::Document.new(src)
-    images = post.images
-    title = post.title
-    leader = post.lede # This is the first couple sentences.
-    keywords = post.keywords(
-      :minimum_score => 1,
-      :stem_at => 2,
-      :word_length_limit => 30,
-      :limit => 500
-    )
-    #oct_vec = keywords.map do |pair|
-    #  Backend::Token.new(:t => pair[0], :f => pair[1])
-    #end
 
-    # TODO: Save the post to the db to get the second param and get the first from the current user
-    # THRIFTCLIENT.addPost(1, 1, oct_vec)
-
-    respond_to do |format|
-      format.html { render :text => [images, title, leader, keywords] }
+    if keywords.empty?
+      return redirect_to new_post_url, :flash => {
+        :error => "Could not determine article keywords!"
+      }
     end
 
-    # redirect_to root_url
+    oct_vec = keywords.map do |pair|
+      Backend::Token.new(:t => pair[0], :f => pair[1])
+    end
+
+    # TODO: Save the post to the db to get the second param and get the first from the current user
+    THRIFTCLIENT.addPost(1, 1, oct_vec)
+
+    redirect_to root_url, :flash => {
+      :notice => "Success! Your article has been submitted."
+    }
   end
 end
