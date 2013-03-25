c0d67fda8ff4ac8715e7f2216ac6a6253542f10e
Tom Dooner <tom@causes.com>
2013-03-25 01:15:05 -0400

    Clear up the difference between Post vs. Posts
    Post => Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,33 @@ class PostsController < ApplicationController
   end
 
   def create
-    flash[:notice] = "Okay, you have posted your article!"
+    begin
+      src = open(params[:url]).read
+    rescue
+      return redirect_to new_post_url, :flash => {
+        :error => 'Could not fetch article.'
+      }
+    end
+    post = Pismo::Document.new(src)
+    # images = post.images
+    # title = post.title
+    # leader = post.lede # This is the first couple sentences.
+    keywords = post.keywords(
+      :minimum_score => 1,
+      :stem_at => 2,
+      :word_length_limit => 30,
+      :limit => 500
+    )
+    oct_vec = keywords.map do |pair|
+      Backend::Token.new(:t => pair[0], :f => pair[1])
+    end
+
+    # TODO: Save the post to the db to get the second param and get the first from the current user
+    THRIFTCLIENT.addPost(1, 1, oct_vec)
+
+    respond_to do |format|
+      format.html { render :text => "Posted\n" }
+    end
 
     redirect_to root_url
   end
