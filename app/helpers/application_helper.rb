3b53bbd417e628ed4509a71477734d0a78e44c09
Tom Dooner <tom@causes.com>
2013-04-09 11:51:58 -0400

    Fix gravatar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def gravatar_url(user, size=128)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
+    "//gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
   end
 end
