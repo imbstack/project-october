5616cda604ff80dbafa3a3d3f86c7d89dfc52122
Tom Dooner <tom@causes.com>
2013-04-15 00:58:24 -0400

    Add reporting of following relationships to the backend

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,3 +1,10 @@
 class Following < ActiveRecord::Base
   attr_accessible :follower_id, :following_id
+
+  after_create :backend_create
+
+private
+  def backend_create
+    THRIFTCLIENT.userToUser(self.follower_id, Backend::Action::FOLLOW, self.following_id)
+  end
 end
